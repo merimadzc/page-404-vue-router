76796be9bb9a4aa75f51cf21d522d7fb7115eedc
Merima Dzinovic <merimadzc@gmail.com>
2019-02-03 12:14:29 +0100

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+/* vue-router */
+import Router from 'vue-router'
+/* Components */
+import HelloWorld from './components/HelloWorld.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    { path: '/', component: HelloWorld },
+   // { path: '*', component: PageNotFound }
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
